57a19eede370b0db7aa2a4efd818bb2baacf3224
Samuel Gutierrez Jaramillo <sgutierr28@eafit.edu.co>
2025-03-15 00:01:00 -0500

    Cart model created

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Carrito
 
 # Register your models here.
+@admin.register(Carrito)
+class CarritoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'subtotal')
+    
+    def subtotal(self, obj):
+        return obj.calcular_subtotal()
+    subtotal.short_description = 'Subtotal'
--- carrito/models.py
+++ carrito/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from menu.models import Plato, Bebida
+from django.conf import settings
 
 # Create your models here.
+class Carrito(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    platos = models.ManyToManyField(Plato, through='CarritoPlato')
+    bebidas = models.ManyToManyField(Bebida, through='CarritoBebida')
+    
+    def calcular_subtotal(self):
+        subtotal_platos = sum(item.plato.precio * item.cantidad for item in self.carritoplato_set.all())
+        subtotal_bebidas = sum(item.bebida.precio * item.cantidad for item in self.carritobebida_set.all())
+        return subtotal_platos + subtotal_bebidas
+    
+class CarritoPlato(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    plato = models.ForeignKey(Plato, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+    
+class CarritoBebida(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    bebida = models.ForeignKey(Bebida, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
